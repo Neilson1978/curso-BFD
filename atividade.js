4242d2d0a2158413f222894aed0d3f4c4c994a25
Neilson1978 <neilson.sm@hotmail.com>
2025-09-27 10:40:07 -0300

    Add files via upload

---
+++ atividade.js
@@ -0,0 +1,20 @@
+/*let musicas = ["primeiros erros" , "giz" , "o segundo sol"];
+console.log(musicas[0]); 
+console.log(musicas[1]); 
+console.log(musicas[2]); 
+musicas.sort();
+console.log(musicas);*/
+/*let tarefas = ["estudar" , "treinar"];
+console.log(tarefas);
+tarefas.push("ler um livro");
+console.log(tarefas);
+tarefas.unshift("tomar café");
+console.log(tarefas);
+tarefas.pop();
+console.log(tarefas);
+tarefas.shift();
+console.log(tarefas);*/
+let amigos = ["Ronilson", "Raimundo", "Francis"];
+for (let i=0; i<amigos.length;i++) {
+    console.log("Oi " + amigos[i] +"! Bem vindos ao site!");
+}
